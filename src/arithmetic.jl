2b7ab4a612b2ebe5ce37ff9a139be1301aedba8f
Marcelo Forets <mforets@gmail.com>
2019-08-08 16:29:55 +0200

    Update src/arithmetic.jl
    Co-Authored-By: Christian Schilling <schillic@informatik.uni-freiburg.de>

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -7,7 +7,7 @@ import Base: +, *
 +(M1::IntervalMatrix, M2::IntervalMatrix) = IntervalMatrix(M1.mat + M2.mat)
 
 +(x::Interval, M::IntervalMatrix) = IntervalMatrix(x .+ M.mat)
-+(M::IntervalMatrix, x::Interval) = +(x, M)
++(M::IntervalMatrix, x::Interval) = IntervalMatrix(x .+ M.mat)
 
 +(x::Number, M::IntervalMatrix) = +(Interval(x), M)
 +(M::IntervalMatrix, x::Number) = +(Interval(x), M)
