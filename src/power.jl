47b88e9ba9f6cf420e8e2f529a2a0d29e8fb8b42
schillic <schillic@informatik.uni-freiburg.de>
2020-01-27 18:04:48 +0100

    tmp

--- src/power.jl
+++ src/power.jl
@@ -6,10 +6,12 @@ function ^(M::IntervalMatrix{T}, k::Integer) where {T}
     # create symbolic matrix
     Ms = Matrix{SymEngine.Basic}(undef, n, n)
     dict = Dict{SymEngine.Basic, Interval{T}}()
+    dict2 = Dict{SymEngine.Basic, T}()
     for i in 1:n, j in 1:n
         var = SymEngine.symbols("M_$(i)_$(j)")
         Ms[i, j] = var
         dict[var] = M[i, j]
+        dict2[var] = mid(M[i, j])
     end
 
     # compute symbolic matrix power
@@ -18,10 +20,12 @@ function ^(M::IntervalMatrix{T}, k::Integer) where {T}
     # substitute intervals in symbolic result
     Mᵏ = similar(M)
     for i in 1:n, j in 1:n
-        println(Msᵏ[i, j])
-        println(dict)
-        substituted = SymEngine.subs(Msᵏ[i, j], dict)
-        Mᵏ[i, j] = SymEngine.lambdify(substituted)
+        expr = Msᵏ[i, j]
+#         for (k, v) in dict
+#             expr = SymEngine.subs(expr, k, mid(v))
+#         end
+        expr = SymEngine.subs(expr, dict2...)
+        Mᵏ[i, j] = SymEngine.lambdify(expr)
     end
     return Mᵏ
 end
