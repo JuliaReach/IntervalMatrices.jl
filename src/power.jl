ab19da215f8d104aa69af60d3ec35051e8283e7f
schillic <schillic@informatik.uni-freiburg.de>
2020-01-27 17:28:30 +0100

    tmp

--- src/power.jl
+++ src/power.jl
@@ -2,16 +2,25 @@ import Base: ^
 
 function ^(M::IntervalMatrix{T}, k::Integer) where {T}
     n = checksquare(M)
-#     Ms = [SymEngine.symbols("M_$(i)_$(j)") for i in 1:n, j in 1:n]
+
+    # create symbolic matrix
     Ms = Matrix{SymEngine.Basic}(undef, n, n)
-    dict = Dict{SymEngine.Basic, T}()
+    dict = Dict{SymEngine.Basic, Interval{T}}()
     for i in 1:n, j in 1:n
         var = SymEngine.symbols("M_$(i)_$(j)")
         Ms[i, j] = var
         dict[var] = M[i, j]
     end
-    println(Ms)
+
+    # compute symbolic matrix power
     Msᵏ = Ms^k
-    Mᵏ = subs(Msᵏ, dict)
+
+    # substitute intervals in symbolic result
+    Mᵏ = similar(M)
+    for i in 1:n, j in 1:n
+        println(Msᵏ[i, j])
+        println(dict)
+        Mᵏ[i, j] = SymEngine.subs(Msᵏ[i, j], dict)
+    end
     return Mᵏ
 end
