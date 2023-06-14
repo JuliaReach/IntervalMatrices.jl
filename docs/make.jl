bd5de59faa27f4b15949fdfdf3d590727b1d3c35
schillic <git@christianschilling.net>
2023-06-14 06:18:21 +0200

    format docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,24 +3,16 @@ using Documenter, IntervalMatrices
 DocMeta.setdocmeta!(IntervalMatrices, :DocTestSetup, :(using IntervalMatrices);
                     recursive=true)
 
-makedocs(
-    sitename = "IntervalMatrices.jl",
-    modules = [IntervalMatrices],
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/aligned.css"], sidebar_sitename=true),
-    strict = true,
-    pages = [
-        "Home" => "index.md",
-        "Library" => Any[
-        "Types" => "lib/types.md",
-        "Methods" => "lib/methods.md"],
-        "About" => "about.md",
-        "References" => "references.md"
-    ]
-)
+makedocs(; sitename="IntervalMatrices.jl",
+         modules=[IntervalMatrices],
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
+                                assets=["assets/aligned.css"], sidebar_sitename=true),
+         strict=true,
+         pages=["Home" => "index.md",
+                "Library" => Any["Types" => "lib/types.md",
+                                 "Methods" => "lib/methods.md"],
+                "About" => "about.md",
+                "References" => "references.md"])
 
-deploydocs(
-    repo = "github.com/JuliaReach/IntervalMatrices.jl.git",
-    push_preview = true,
-)
+deploydocs(; repo="github.com/JuliaReach/IntervalMatrices.jl.git",
+           push_preview=true)
